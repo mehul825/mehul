cf77f811312c499f0561a3d7d1b3e28589e2beb9
Md Qutubuddin Ansari <fcharlie137@gmail.com>
2023-12-24 17:29:02 +0530

    first commit

---
+++ Restaurant/js/index.js
@@ -0,0 +1,15 @@
+// var hamburger = document.querySelector('.hamburger');
+// var a = document.querySelector('a');
+
+// hamburger.addEventListener('click', () => {
+//     ul.classList.toggle('ham_active');
+
+//     let ham_child = hamburger.firstElementChild;
+//     if (ul.classList.contains('ham_active')) {
+//         ham_child.classList.replace('fa-bars', 'fa-xmark');
+//     }
+//     else {
+//         ham_child.classList.replace('fa-xmark', 'fa-bars');
+
+//     }
+// })
